a8eef3f22f0ec65d0fe2421949996da1e5474c6e
lerroy-lee <lerroyogari@gmail.com>
2021-07-20 23:02:07 +0300

    myfirstIP

---
+++ scriptfile.js/name.js
@@ -0,0 +1,60 @@
+//Receive Inputs 
+function getInputVals()
+{
+  function getInputVals()
+
+  const dd = document.getElementById("day").nodeValue;
+  const mm = document.getElementById("month").nodeValue;
+  const yr = document.getElementById("year").nodeValue;
+  //get the value of the  gender radio buttons 
+  const gender= document.querySelector('input[name="gender"]:checked').nodeValue;
+   //const gender= document.getElementById("gender").nodevalue
+
+//get the value of tthe radio buttons 
+  console.log( +dd++ "/"+mm+"/"+yr+" "+ gender);
+   //Check if all inputs fieldds have been entered
+   if(dd==""){
+     alert("The day field cannot be empty")
+     return false;
+   }
+  {
+    if(mm=="")
+    alert("The month field cannot be empty")
+    return false;
+  }
+}
+switch (malename){
+  case 0:
+    print("kwasi");
+    case 1:
+    print("kwaduo");
+    case 2:
+    print("kwabena");
+    case 3:
+    print("kwaku");
+    case 4:
+    print("yaw");
+    case 5:
+    print("kofi");
+    case 6:
+    print("kwame");
+}
+console.log(print)
+
+switch (femalename){
+  case 0:
+    print("akosua");
+    case 1:
+    print("adwoa");
+    case 2:
+    print("abenaa");
+    case 3:
+    print("akua");
+    case 4:
+    print("yaa");
+    case 5:
+    print("akua");
+    case 6:
+    print("ama");
+}
+console.log(print)
